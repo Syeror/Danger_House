00dff30a69a04230ac3108aeaf000ad65793ebc1
Syeror <stavrporo@gmail.com>
2024-05-06 21:01:14 +0300

    2

--- Assets/Stavr/ItemPickup.cs
+++ Assets/Stavr/ItemPickup.cs
@@ -8,10 +8,11 @@ public class ItemPickup : MonoBehaviour
 {
     [SerializeField] private GameObject itemPrefab; // Префаб объекта, который будет подбираться
     [SerializeField] private float pickupDistance = 2f; // Расстояние на котором можно подобрать предмет
-    public TextMeshProUGUI NumbersKeysTMP;
+    public TMP_Text NumbersKeysTMP;
     public int AllKeys;
-    [Range(1, 4)]
-    public int Keys = 1;
+    [Range(0, 4)]
+    public int Keys = 0;
+
 
 
     private void Update()
@@ -37,15 +38,15 @@ public class ItemPickup : MonoBehaviour
     {
         // Уничтожаем объект, который был подобран
         Destroy(pickableItem.gameObject);
-
-        NumbersKeysTMP.text = Keys.ToString();
         Keys++;
-        if (Keys == 5)
+        NumbersKeysTMP.text = Keys.ToString();
+
+        if (Keys == 4)
         {
             SceneManager.LoadScene(2);
         }
         // Создаем новый объект префаба в позиции игрока
-        Instantiate(itemPrefab, transform.position, transform.rotation);
+        //Instantiate(itemPrefab, transform.position, transform.rotation);
 
         // Выполняем действия, связанные с подбором предмета (например, добавить его в инвентарь)
         // ...
