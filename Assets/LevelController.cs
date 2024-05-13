b6e7a91fbabaac56cd446010d0a577ab8fab8742
Syeror <stavrporo@gmail.com>
2024-05-13 17:12:46 +0300

    PauseTheGameDone
    3!!!!!

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -7,18 +7,30 @@ public class LevelController : MonoBehaviour
 {
     // Flag to indicate if the game is paused
     private bool isPaused = false;
+    public GameObject PauseCanvas;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        PauseCanvas.SetActive(false);
+    }
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePauseGame();
+        }
+        if(isPaused == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Q))
+            {
+                SceneManager.LoadScene(0);
+            }
         }
 
     }
@@ -59,16 +71,26 @@ public class LevelController : MonoBehaviour
         {
             // Pause the game (e.g., set Time.timeScale to 0)
             Time.timeScale = 0f;
-
+            PauseCanvas.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
             // Optionally, display a pause menu or visual indicator
         }
         else
         {
             // Resume the game (e.g., set Time.timeScale back to 1)
             Time.timeScale = 1f;
-
+            PauseCanvas.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
             // Optionally, hide the pause menu or visual indicator
         }
+    }
+    public void GettingBackIntoTheGame()
+    {
+        // Resume the game (e.g., set Time.timeScale back to 1)
+        Time.timeScale = 1f;
+        PauseCanvas.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        // Optionally, hide the pause menu or visual indicator
     }
 }
 
